7b0b18a4619defc935b8a71822e5e8f747852176
StefanopuloZ <deakstefan@gmail.com>
2021-08-06 10:25:09 +0200

    part 2 course start

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,15 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage()
+    public function homepage(Environment $twigEnvironment)
     {
+        /*
+        // fun example of using the Twig service directly!
+        $html = $twigEnvironment->render('question/homepage.html.twig');
+
+        return new Response($html);
+        */
+
         return $this->render('question/homepage.html.twig');
     }
 
@@ -26,11 +35,9 @@ class QuestionController extends AbstractController
             'Maybe... try saying the spell backwards?',
         ];
 
-        dump($slug, $this);
-
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
-            'answers' => $answers
+            'answers' => $answers,
         ]);
     }
 }
