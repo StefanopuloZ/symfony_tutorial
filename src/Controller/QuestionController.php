041b5c9250ac1e33f614e3427cf58be6ab2ee60e
StefanopuloZ <deakstefan@gmail.com>
2021-08-08 12:20:12 +0200

    update

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,9 +3,7 @@
 namespace App\Controller;
 
 use App\Service\MarkdownHelper;
-use Exception;
 use Psr\Log\LoggerInterface;
-use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -39,15 +37,11 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper, HubInterface $sentryHub)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
-        dump($sentryHub);
-
         if ($this->isDebug === true) {
             $this->logger->info('We are in debug mode!');
         }
-
-        // throw new \Exception('Bad stuff happened!');
 
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
